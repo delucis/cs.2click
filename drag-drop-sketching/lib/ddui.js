13dbdff46ff7738ec2f319b4b1ee9f3fccdc64aa
delucis <swithinbank@gmail.com>
2015-03-13 12:23:17 -0400

    ddui.js now generates [textbutton] objects in response to module message

--- drag-drop-sketching/lib/ddui.js
+++ drag-drop-sketching/lib/ddui.js
@@ -15,7 +15,7 @@ var modulelist = '';
 var modulenum = 0;
 
 // Maxobj variables for scripting
-var trackInfo = new Array(32);
+var dduiModules = new Array(32);
 
 /*
 // methods start here
@@ -37,14 +37,21 @@ function modules(val)
 		a = arguments;
 		modulenum = a.length;
 		modulelist = a;
+		
+		// print number of modules
 		post(modulenum);
 		post();
 		
 		// iterate args
 		for(k=0;k<modulenum;k++)
 		{
+			// print module names one by one
 			post(modulelist[k]);
 			post();
+			
+			x = 165; // set object’s x co-ordinate
+			y = 30 * (k+1); // set object’s y co-ordinate
+			dduiModules[k] = this.patcher.newdefault(x, y, "textbutton", "@varname", "ddui-" + k, "@patching_rect", x, y, 150, 30, "@text", "⁝" + modulelist[k], "@fontname", "Arial", "@fontsize", 13., "@align", "left", "@bgcolor", 1., 1., 1., 1., "@border", 2, "@rounded", 0);
 		}
 		
 		/*
