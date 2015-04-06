55dfb9c296f7672ec2565530dae59d2785cfa956
delucis <swithinbank@gmail.com>
2015-04-06 18:30:18 -0400

    Moved clear button creation/deletion to separate function
    addclearbutton(2 out); — adds a clear button to the 2nd slot of an
    output module
    removeclearbutton(3 in); — removes a clear button from the 3rd slot of
    an input module

--- cs.two-click-router/lib/cs.2click.js
+++ cs.two-click-router/lib/cs.2click.js
@@ -61,7 +61,6 @@ function buildslots(val)
 			twoclickObjects[4*k+3] = this.patcher.newdefault( x, y, "ubutton", "@varname", "#1-" + (k+1) + "-" + chantype + "-slot", "@patching_rect", x, y, 105, 15, "@presentation_rect", x, y, 105, 15, "@presentation", 1, "@hltcolor", 0.05, 0.97, 0.39, 0.5, "@rounded", 0);
 			twoclickObjects[4*k] = this.patcher.newdefault( x, y, "textbutton", "@varname", "#1-" + (k+1) + "-" + chantype + "-slot-label", "@patching_rect", x, y, 15, 15, "@presentation_rect", x, y, 15, 15, "@presentation", 1, "@mode", 1, "@text", k+1, "@texton", k+1, "@fontname", "Arial", "@fontsize", 10., "@fontface", 1, "@align", 1, "@bgcolor", 0.25, 0.25, 0.25, 1., "@textcolor", 0.96, 0.96, 0.96, 1., "@border", 1, "@rounded", 0, "@bordercolor", 1., 1., 1., 1., "@bgoncolor", 0.05, 0.97, 0.39, 1., "@textoncolor", 0., 0., 0., 1., "@borderoncolor", 0.05, 0.97, 0.39, 1.);
 			twoclickObjects[4*k+1] = this.patcher.newdefault( x+15, y, "textbutton", "@varname", "#1-" + (k+1) + "-" + chantype + "-slot-val", "@patching_rect", x+15, y, 90, 15, "@presentation_rect", x+15, y, 90, 15, "@presentation", 1, "@mode", 1, "@text", "[empty]", "@texton", "[empty]", "@fontname", "Arial", "@fontsize", 10., "@fontface", 1, "@align", 0, "@bgcolor", 0.96, 0.96, 0.96, 1., "@textcolor", 0.2, 0.2, 0.2, "@border", 1, "@rounded", 0, "@bordercolor", 1., 1., 1., 1., "@bgoncolor", 0.05, 0.97, 0.39, 1., "@textoncolor", 0., 0., 0., 1., "@borderoncolor", 0.05, 0.97, 0.39, 1.);
-			twoclickObjects[4*k+2] = this.patcher.newdefault( x+105, y, "textbutton", "@varname", "#1-" + (k+1) + "-" + chantype + "-slot-clear", "@patching_rect", x+105, y, 15, 15, "@presentation_rect", x+105, y, 15, 15, "@presentation", 1, "@text", "✖", "@fontname", "Arial", "@fontsize", 10., "@fontface", 1, "@align", 1, "@bgcolor", 0.99, 0.41, 0.43, 1., "@textcolor", 1., 1., 1., 1., "@border", 1, "@rounded", 0, "@bordercolor", 1., 1., 1., 1., "@bgoncolor", 0.05, 0.97, 0.39, 1., "@bgovercolor", 1., 0., 0., 1., "@textovercolor", 0., 0., 0., 1. );
 		}
 
 		// done building confirmation out left outlet
@@ -76,6 +75,46 @@ function buildslots(val)
 	else // complain about arguments
 	{
 		error("buildslots message needs arguments:\n1 — number of channels (int); 2 — in or out (symbol)\n");
+	}
+}
+
+// addclearbutton -- add a clear slot button to a provided slot number
+function addclearbutton(val)
+{
+	if(arguments.length)  // bail if no arguments
+	{
+		k = arguments[0] - 1; // set k from slot number
+		chantype = arguments[1]; // set channel type (in/out)
+		scriptingname = "#1-" + (k+1) + "-" + chantype + "-slot-clear";
+		x = 0; // set object’s x co-ordinate
+		y = 15 * (k+1); // set object’s y co-ordinate
+		// execute -- remove if already there, then add
+		removeclearbutton(arguments[0], arguments[1]);
+		twoclickObjects[4*k+2] = this.patcher.newdefault( x+105, y, "textbutton", "@varname", scriptingname, "@patching_rect", x+105, y, 15, 15, "@presentation_rect", x+105, y, 15, 15, "@presentation", 1, "@text", "✖", "@fontname", "Arial", "@fontsize", 10., "@fontface", 1, "@align", 1, "@bgcolor", 0.99, 0.41, 0.43, 1., "@textcolor", 1., 1., 1., 1., "@border", 1, "@rounded", 0, "@bordercolor", 1., 1., 1., 1., "@bgoncolor", 0.05, 0.97, 0.39, 1., "@bgovercolor", 1., 0., 0., 1., "@textovercolor", 0., 0., 0., 1. );
+	}
+	else // complain about arguments
+	{
+		error("addclearbutton message needs arguments:\n1 — number of slot to add clear button to (int); 2 — in or out (symbol)\n");
+	}
+}
+
+// removeclearbutton
+function removeclearbutton(val)
+{
+	if(arguments.length)  // bail if no arguments
+	{
+		k = arguments[0] - 1; // set k from slot number
+		chantype = arguments[1]; // set channel type (in/out)
+		scriptingname = "#1-" + (k+1) + "-" + chantype + "-slot-clear";
+		this.patcher.apply(function(object) {
+			if (object.varname == scriptingname) {
+				this.patcher.remove(object);
+			}
+		});
+	}
+	else // complain about arguments
+	{
+		error("removeclearbutton message needs arguments:\n1 — number of slot to remove clear button from (int); 2 — in or out (symbol)\n");
 	}
 }
 
